8cf120764e044994dc7380b20da250b34eb83ea4
Mateusz Fraczek <matt@universeldev.com>
2017-12-13 12:17:28 +0100

    Initial Commit

---
+++ MumsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MumsApp
+//
+//  Created by Mateusz Fraczek on 13/12/2017.
+//  Copyright © 2017 Mateusz Fraczek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
