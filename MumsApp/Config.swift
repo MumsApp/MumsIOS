66ee61ce45ec4097b54713513364a6da3b4fe9f5
Mateusz Fraczek <matt@universeldev.com>
2018-05-17 14:08:38 +0200

    Added rating to user profile.

--- MumsApp/Config.swift
+++ MumsApp/Config.swift
@@ -3,16 +3,18 @@ import Foundation
 // API
 
 let BASE_PUBLIC_URL = "https://devmumsapp.atomdev.eu/api/public/"
-
 let BASE_URL = "https://devmumsapp.atomdev.eu/api/"
 
 // GOOGLE MAPS
 
 let GOOGLE_MAPS_KEY = "AIzaSyDvwbXTX58A7OWL89MLLiaAXTKqQPDyPrk"
-
 let GOOGLE_SIGNIN_KEY = "158500825071-3mcog1848m2j4pn1k53ckmml6an0fq7j.apps.googleusercontent.com"
+
+// DEFAULT LOCATION
 
 let LONDON_LAT = 51.512750
 let LONDON_LONG = -0.127698
+
+// EMERGENCY NUMBER
 
 let EMERGENCY_NUMBER = "0"
